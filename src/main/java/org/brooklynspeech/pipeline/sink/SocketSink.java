ffc7e75be2967291f54a9adf6c9df79fc841eaeb
Matt <mattm458@gmail.com>
2022-08-08 14:14:15 -0400

    SocketSink sends byte data to remote address

--- src/main/java/org/brooklynspeech/pipeline/sink/SocketSink.java
+++ src/main/java/org/brooklynspeech/pipeline/sink/SocketSink.java
@@ -1,5 +1,6 @@
 package org.brooklynspeech.pipeline.sink;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -8,16 +9,30 @@ import java.util.concurrent.BlockingQueue;
 import org.brooklynspeech.pipeline.core.Sink;
 
 public class SocketSink extends Sink<byte[]> {
+
     private final DatagramSocket socket;
+    private final InetAddress address;
+    private final int port;
 
     public SocketSink(BlockingQueue<byte[]> queue, InetAddress address, int port) throws SocketException {
         super(queue);
-        this.socket = new DatagramSocket(port, address);
+
+        this.socket = new DatagramSocket();
+        this.address = address;
+        this.port = port;
     }
 
     @Override
     public void run() {
-
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                byte[] buffer = this.queue.take();
+                this.socket.send(new DatagramPacket(buffer, buffer.length, this.address, this.port));
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            System.exit(1);
+        }
     }
 
 }
