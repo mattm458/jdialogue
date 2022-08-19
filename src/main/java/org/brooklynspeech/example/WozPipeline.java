2dcd1903bc05d577effb3f478d51870e502d2ae8
Matt <mattm458@gmail.com>
2022-08-19 13:51:19 -0400

    FreeTTS dependencies and tweaks to the example woz pipeline

--- src/main/java/org/brooklynspeech/example/WozPipeline.java
+++ src/main/java/org/brooklynspeech/example/WozPipeline.java
@@ -10,16 +10,16 @@ import org.brooklynspeech.pipeline.source.SocketTextSource;
 import org.brooklynspeech.pipeline.tts.FreeTTSProcessor;
 
 public class WozPipeline {
-    protected static final int SYNTH_OUT_PORT = 9003;
-    protected static final int AUDIO_SINK_PORT = 9002;
-    protected static final int TEXT_SOURCE_PORT = 9001;
+    protected static final int SYNTH_OUT_PORT = 9993;
+    protected static final int AUDIO_SINK_PORT = 9992;
+    protected static final int TEXT_SOURCE_PORT = 9991;
     protected static final int BUFFER_SIZE = 1024;
 
     public static void main(String[] args) throws Exception {
         Conversation<Chunk> context = new Conversation<>(0);
 
         final AudioFileSource audioPipelineSource;
-        audioPipelineSource = new AudioFileSource("wav/GAME_speakerB.wav", BUFFER_SIZE);
+        audioPipelineSource = new AudioFileSource("/wav/GAME_speakerB.wav", BUFFER_SIZE);
 
         final Pipeline<byte[]> audioPipeline = new Pipeline<>(audioPipelineSource)
                 .setSink(new SocketSink(AUDIO_SINK_PORT));
