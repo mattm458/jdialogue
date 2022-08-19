f70543f7e830b98a662b7554837046144341cc6d
Matt <mattm458@gmail.com>
2022-08-19 01:26:37 -0400

    Add AudioSocketForwardProcessor

---
+++ src/main/java/org/brooklynspeech/pipeline/audio/AudioSocketForwardProcessor.java
@@ -0,0 +1,46 @@
+package org.brooklynspeech.pipeline.audio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.brooklynspeech.pipeline.core.PassthroughStreamProcessor;
+
+public class AudioSocketForwardProcessor extends PassthroughStreamProcessor<byte[]> {
+
+    private final int port;
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private OutputStream stream;
+
+    public AudioSocketForwardProcessor(int port) throws IOException {
+        this.port = port;
+    }
+
+    @Override
+    protected void setup() {
+        try {
+            this.serverSocket = new ServerSocket(port);
+            this.socket = this.serverSocket.accept();
+            this.stream = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+            System.exit(1);
+        }
+
+    }
+
+    @Override
+    public byte[] doProcess(byte[] bytes) {
+        try {
+            this.stream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+            System.exit(1);
+        }
+
+        return bytes;
+    }
+}
